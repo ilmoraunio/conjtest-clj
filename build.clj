2122489a02995cec8f6efbb18fdf46a473191237
Ilmo Raunio <ilmo@raunio.email>
2025-03-30 16:08:29 +0300

    Build & deploy scripts

--- build.clj
+++ build.clj
@@ -1,73 +1,45 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.deps :as t]
-            [clojure.tools.build.api :as b]
-            [deps-deploy.deps-deploy :as dd]))
+  (:require [clojure.string :as str]
+            [clojure.tools.build.api :as b]))
 
-(def lib 'net.clojars.ilmoraunio/conjtest)
-(def version "0.1.0-SNAPSHOT")
-#_ ; alternatively, use MAJOR.MINOR.COMMITS:
-(def version (format "1.0.%s" (b/git-count-revs nil)))
-(def class-dir "target/classes")
+(def build-folder "target")
+(def jar-content (str build-folder "/classes"))     ; folder where we collect files to pack in a jar
 
-(defn test "Run all the tests." [opts]
-  (println "\nRunning tests...")
-  (let [basis    (b/create-basis {:aliases [:test]})
-        combined (t/combine-aliases basis [:test])
-        cmds     (b/java-command
-                  {:basis basis
-                   :java-opts (:jvm-opts combined)
-                   :main      'clojure.main
-                   :main-args ["-m" "cognitect.test-runner"]})
-        {:keys [exit]} (b/process cmds)]
-    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
-  opts)
+(def lib-name 'com.github.ilmoraunio/conjtest)
+(def version (-> (slurp "resources/CONJTEST_LIB_VERSION")
+                 str/trim))
+(def is-release (Boolean/parseBoolean (System/getenv "RELEASE")))
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file-name (format "%s/%s.jar" build-folder (name lib-name)))
 
-(defn- pom-template [version]
-  [[:description "FIXME: my new library."]
-   [:url "https://github.com/ilmoraunio/conjtest"]
-   [:licenses
-    [:license
-     [:name "Eclipse Public License"]
-     [:url "http://www.eclipse.org/legal/epl-v10.html"]]]
-   [:developers
-    [:developer
-     [:name "Ilmo.raunio"]]]
-   [:scm
-    [:url "https://github.com/ilmoraunio/conjtest"]
-    [:connection "scm:git:https://github.com/ilmoraunio/conjtest.git"]
-    [:developerConnection "scm:git:ssh:git@github.com:ilmoraunio/conjtest.git"]
-    [:tag (str "v" version)]]])
+(defn clean
+  [_]
+  (b/delete {:path build-folder})
+  (println (format "Build folder \"%s\" removed" build-folder)))
 
-(defn- jar-opts [opts]
-  (assoc opts
-         :lib lib   :version version
-         :jar-file  (format "target/%s-%s.jar" lib version)
-         :basis     (b/create-basis {})
-         :class-dir class-dir
-         :target    "target"
-         :src-dirs  ["src"]
-         :pom-data  (pom-template version)))
-
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (test opts)
-  (b/delete {:path "target"})
-  (let [opts (jar-opts opts)]
-    (println "\nWriting pom.xml...")
-    (b/write-pom opts)
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
-    (println "\nBuilding JAR...")
-    (b/jar opts))
-  opts)
-
-(defn install "Install the JAR locally." [opts]
-  (let [opts (jar-opts opts)]
-    (b/install opts))
-  opts)
-
-(defn deploy "Deploy the JAR to Clojars." [opts]
-  (let [{:keys [jar-file] :as opts} (jar-opts opts)]
-    (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
-                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))}))
-  opts)
+(defn jar
+  [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs   ["src"]
+               :target-dir jar-content})
+  (b/write-pom {:class-dir jar-content
+                :lib       lib-name
+                :version   (if is-release
+                             version
+                             (str version "-SNAPSHOT"))
+                :basis     basis
+                :src-dirs  ["src"]
+                :scm {:url "https://github.com/ilmoraunio/conjtest"
+                      :connection "scm:git:git://github.com/ilmoraunio/conjtest.git"
+                      :tag (if is-release
+                             version
+                             (b/git-process {:git-args "rev-parse HEAD"}))}
+                :pom-data [[:licenses
+                            [:license
+                             [:name "The MIT License (MIT)"]
+                             [:url "https://mit-license.org/"]
+                             [:distribution "repo"]]]]})
+  (b/jar {:class-dir jar-content
+          :jar-file jar-file-name})
+  (println (format "Jar file created: \"%s\"" jar-file-name)))
